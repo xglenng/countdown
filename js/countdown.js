dcbd1c9041dd65511f2c9968ca94458271d8cb46
xglenng <xglenng@gmail.com>
2015-01-29 01:50:52 -0700

    Garrett GLenn CountDown assignment

--- js/countdown.js
+++ js/countdown.js
@@ -1,11 +1,30 @@
 //change the text below to reflect your own,
-var before = "Groundhog's Day!";
-var current = "Today is Groundhog's Day!";
-var montharray = new Array("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec");
+var current = "Happy Birthday Chris and Nancy!";
+var monthArray = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+var seconds = 1000;
+var minutes = seconds*60;
+var hours = minutes*60;
+var days = hours*24;
 
 function countdown(yr, m, d) {
- // ...
-}
 
+	year = yr; month = m; day = d;
+	var today = new Date();
+	var futureDate = monthArray[m-1] +" "+ d +", "+ yr;
+	var difference = Date.parse(futureDate) - Date.parse(today);
+	var dayRemaining = Math.floor(difference/days);
+	var hourRemaining = Math.floor((difference%days)/hours);
+	var minRemaining = Math.floor(((difference%days)%hours)/minutes);
+	var secRemaining = Math.floor((((difference%days)%hours)%minutes)/seconds);
+
+	if(dayRemaining == 0 && hourRemaining == 0 && minRemaining == 0 && secRemaining == 1){
+		document.forms.count.count2.value = current;
+		return
+	}
+	else{
+		document.forms.count.count2.value = dayRemaining +" days, "+ hourRemaining +" hours, "+ minRemaining +" minutes, and "+ secRemaining +" secs";
+		setTimeout("countdown(year, month, day)",1000);
+	}
+}
 //enter the count down date using the format year/month/day
-countdown(2015, 2, 2);
+countdown(2015, 2, 18);
--- php/countdown.php
+++ php/countdown.php
@@ -1,15 +1,14 @@
 <?php
-
 if (PHP_SAPI !== 'cli') {
     die (PHP_EOL.'Please run '.__FILE__.' from the command-line.'.PHP_EOL.PHP_EOL);
 }
-
 function countdown($seconds)
 {
-    // ...
+    for($i = $seconds; $i > 0; $i--){
+		echo $i."\n";
+		sleep(1);
+	}
 }
-
-countdown(5);
+countdown(10);
 
 die('0 All done...');
-echo 'hello';
